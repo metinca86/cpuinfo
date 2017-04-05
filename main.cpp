2a62ac62c94e825555399b9cb8015dc23fcb8ec0
metinca86 <metin.cankaya@live.de>
2017-04-05 19:08:13 +0200

    Add files via upload
    Add Application Icon

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QIcon>
 
 #include "cpuinfomodel.h"
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
+    app.setWindowIcon(QIcon(":/cpuinfo.ico"));
 
     qmlRegisterType<CpuInfoModel>("Backend", 1, 0, "CpuInfoModel");
 
